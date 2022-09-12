4b8a14479ce62d506ef33941190d424885f14f49
Bailey <bailey.mann@vtsd.com>
2022-09-12 11:44:02 -0400

    My first commit.

--- DataTypes.java
+++ DataTypes.java
@@ -7,10 +7,21 @@
 
  */
 public class DataTypes {
+    public static void main(String args[]){
+        // Variable syntax: dataType varName = value;
+        int baileyFavInt = 8;
+        double snicker = 7.5;
+        boolean likeDogs = true;
+
+        System.out.println("Bailey's Favorite Integer is:" + baileyFavInt + " and the class snicker was " + snicker);
+
+        int n1 = 1;
+        int n2 = 2;
+
+        System.out.print(n1 + n2 / n1);
+
+    }
 
 
-    // Variable syntax: dataType varName = value'
-    int baileyFavInt = 8;
-    double snicker = 7.5;
-    boolean likeDogs = true;
+
 }
